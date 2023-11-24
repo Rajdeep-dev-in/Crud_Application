0271a18d64fbb50b13e2381301b54ce164fb271f
Rajdeep-dev-in <rajdeepdev03@gmail.com>
2023-11-25 02:12:53 +0530

    create files

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include('./header.php') ?>
+        <div class="heading">
+            <h2>Student Details</h2>
+        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First_Name</th>
+                    <th>Last_Name</th>
+                    <th>Age</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Rajdeep</td>
+                    <td>Das</td>
+                    <td>23</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Bishal</td>
+                    <td>Roy</td>
+                    <td>23</td>
+                </tr>
+            </tbody>
+        </table>
+<?php include('./footer.php') ?>  
